e3ba6aad517cd4db9b49f2532b67fedbbc36ea7d
Gabriel Schützeneder <g.schuetzeneder@gmail.com>
2016-05-03 20:20:39 +0200

    Removed title bar. (Status bar remained) Application now runnable, Changed layout a little bit.

---
+++ app/src/main/java/mc004_035/rating_app/Homescreen.java
@@ -0,0 +1,34 @@
+package mc004_035.rating_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+
+
+/**
+ * Created by Gabriel on 03.05.16.
+ */
+public class Homescreen extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+
+
+    }
+
+//    @Override
+//    public void onClick(View v) {
+//        int id=v.getId();
+//        switch (id) {
+//            case
+//        }
+
+
+}
