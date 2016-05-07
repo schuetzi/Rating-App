6b844e64d7cf8f0f86a9ec55a6d81f74d93c8bac
Gabriel Schützeneder <g.schuetzeneder@gmail.com>
2016-05-07 19:50:17 +0200

    Added a new Layout: Qualität. --> Already working on "Einfaches Handling"

--- app/src/main/java/mc004_035/rating_app/Homescreen.java
+++ app/src/main/java/mc004_035/rating_app/Homescreen.java
@@ -1,14 +1,26 @@
 package mc004_035.rating_app;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
-import android.graphics.PixelFormat;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+import android.widget.ViewFlipper;
 
 
 /**
@@ -17,6 +29,8 @@ import android.widget.VideoView;
 public class Homescreen extends Activity {
 
     ImageButton imageButton;
+    private ViewFlipper viewFlipper;
+    private float lastX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +64,127 @@ public class Homescreen extends Activity {
     public void getHomescreen(View v) {
         setContentView(R.layout.homescreen);
     }
+
+    public void getQualitaet(View v) {
+        setContentView(R.layout.qualitaet);
+    }
+
+    public void getHandling(View v) {
+        setContentView(R.layout.handling);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+    }
+
+    public void setAnimationFade(View v, int id) {
+        final ImageButton myButton = (ImageButton) findViewById(id);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(myButton, View.ALPHA, 0.5f, 1f);
+        animator.setDuration(300); //ms
+        animator.start();
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                myButton.setAlpha(1f);
+            }
+        });
+    }
+
+    public void getAnimationFade(View v) {
+        TextView text;
+        switch (v.getId()) {
+            case R.id.ButtonBauer1:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer1);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer1);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer2:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer2);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer2);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer3:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer3);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer3);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer4:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer4);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer4);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer5:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer5);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer5);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer6:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer6);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer6);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer7:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer7);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer7);
+                text.setVisibility(v.VISIBLE);
+                break;
+            case R.id.ButtonBauer8:
+                setAnimationFade(v, v.getId());
+                text = (TextView) findViewById(R.id.NameBauer8);
+                text.setVisibility(v.VISIBLE);
+                text = (TextView) findViewById(R.id.OrtBauer8);
+                text.setVisibility(v.VISIBLE);
+                break;
+        }
+    }
+
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = touchevent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                float currentX = touchevent.getX();
+                // Handling left to right screen swap.
+                if (lastX < currentX) {
+                    // If there aren't any other children, just break.
+                    if (viewFlipper.getDisplayedChild() == 0)
+                    break;
+                    // Next screen comes in from left.
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_left);
+                    // Current screen goes out from right.
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_right);
+                    // Display next screen.
+                    viewFlipper.showNext();
+                }
+                // Handling right to left screen swap.
+                if (lastX > currentX) {
+                    // If there is a child (to the left), kust break.
+                    if (viewFlipper.getDisplayedChild() == 1)
+                    break;
+                    // Next screen comes in from right.
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_right);
+                    // Current screen goes out from left.
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_left);
+                    // Display previous screen.
+                    viewFlipper.showPrevious();
+                }
+                break;
+        }
+        return false;
+    }
+
 
 
 }
