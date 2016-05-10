b87d690682d24a05635542fc0ae4ff3067c957e9
Gabriel Schützeneder <g.schuetzeneder@gmail.com>
2016-05-10 17:02:01 +0200

    modified all layouts for tablet

--- app/src/main/java/mc004_035/rating_app/Homescreen.java
+++ app/src/main/java/mc004_035/rating_app/Homescreen.java
@@ -154,42 +154,44 @@ public class Homescreen extends Activity {
         }
     }
 
-    /*public boolean onTouchEvent(MotionEvent touch) {
-        switch (touch.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                lastX = touch.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                float currentX = touch.getX();
-                // Handling left to right screen swap.
-                if (lastX < currentX) {
-                    // If there aren't any other children, just break.
-                    if (viewFlipper.getDisplayedChild() == 0)
+    public boolean onTouchEvent(MotionEvent touch) {
+        if (viewFlipper != null) {
+            switch (touch.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    lastX = touch.getX();
                     break;
-                    // Next screen comes in from left.
-                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_left);
-                    // Current screen goes out from right.
-                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_right);
-                    // Display next screen.
-                    viewFlipper.showNext();
-                }
-                // Handling right to left screen swap.
-                if (lastX > currentX) {
-                    // If there is a child (to the left), just break.
-                    if (viewFlipper.getDisplayedChild() == 1)
+                case MotionEvent.ACTION_UP:
+                    float currentX = touch.getX();
+                    // Handling left to right screen swap.
+                    if (lastX < currentX) {
+                        // If there aren't any other children, just break.
+                        if (viewFlipper.getDisplayedChild() == 0)
+                            break;
+                        // Next screen comes in from left.
+                        viewFlipper.setInAnimation(this, R.anim.slide_in_from_left);
+                        // Current screen goes out from right.
+                        viewFlipper.setOutAnimation(this, R.anim.slide_out_to_right);
+                        // Display next screen.
+                        viewFlipper.showNext();
+                    }
+                    // Handling right to left screen swap.
+                    if (lastX > currentX) {
+                        // If there is a child (to the left), just break.
+                        if (viewFlipper.getDisplayedChild() == 1)
+                            break;
+                        // Next screen comes in from right.
+                        viewFlipper.setInAnimation(this, R.anim.slide_in_from_right);
+                        // Current screen goes out from left.
+                        viewFlipper.setOutAnimation(this, R.anim.slide_out_to_left);
+                        // Display previous screen.
+                        viewFlipper.showPrevious();
+                    }
                     break;
-                    // Next screen comes in from right.
-                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_right);
-                    // Current screen goes out from left.
-                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_left);
-                    // Display previous screen.
-                    viewFlipper.showPrevious();
-                }
-                break;
+            }
         }
         return false;
-    }*/
 
+    }
 
 
 }
